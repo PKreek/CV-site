3d7fcc29ca5f5f979b4dffd948b64535ab3c8683
noahjarvback <noah.jarvback@hotmail.com>
2022-12-14 14:15:37 +0100

    mellanslag

--- CV-Site MVC/Models/ErrorViewModel.cs
+++ CV-Site MVC/Models/ErrorViewModel.cs
@@ -4,8 +4,8 @@ namespace CV_Site_MVC.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+         public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
