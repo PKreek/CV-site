60ffaf9a71edd36dc9d90802a0cb7185f9eb0aa7
markwest7 <markus.westberg@gmail.com>
2022-12-15 12:59:41 +0100

    gtraa

---
+++ CV-Site MVC/Models/Projekt.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace CV_Site_MVC.Models
+{
+    public class Projekt
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public DateAndTime StartDate { get; set; }
+    }
+}
---
+++ CV-Site MVC/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace CV_Site_MVC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        //Password?
+
+    }
+}
