eecb4fa51a4877f2604b703cb606bc180ac64972
markwest7 <markus.westberg@gmail.com>
2022-12-16 12:29:31 +0100

    sida med projekt

--- CV-Site MVC/Controllers/HomeController.cs
+++ CV-Site MVC/Controllers/HomeController.cs
@@ -11,27 +11,39 @@ namespace CV_Site_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private SiteContext _dbContext;
+
+        public HomeController(SiteContext dbContext)
         {
-            _logger = logger;
+            _dbContext = dbContext;
         }
+
+
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
-            return View();
+            List<Project> listOfProjects = _dbContext.Projects.ToList();
+            ViewBag.Message = "Hej";
+            return View(listOfProjects);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
