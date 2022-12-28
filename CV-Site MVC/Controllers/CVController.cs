d6d934deb6c05040b11d1a8116044cb3f968c839
markwest7 <markus.westberg@gmail.com>
2022-12-28 15:06:21 +0100

    tillfällig CV knapp och Cv sida

---
+++ CV-Site MVC/Controllers/CVController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Site_MVC.Controllers
+{
+    public class CVController : Controller
+    {
+        public IActionResult CV()
+        {
+            return View();
+        }
+    }
+}
