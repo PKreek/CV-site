36f203c8db0356b59e9009ecf831eac1b3d9c628
markwest7 <markus.westberg@gmail.com>
2022-12-29 16:32:58 +0100

    Ganska snyggt CV

--- CV-Site MVC/Controllers/CVController.cs
+++ CV-Site MVC/Controllers/CVController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CV_Site_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CV_Site_MVC.Controllers
 {
     public class CVController : Controller
     {
+        private SiteContext _dbContext;
+
+        public CVController(SiteContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult CV()
         {
-            return View();
+            CvViewModel model = new CvViewModel();
+            model.Cv = _dbContext.cVs.Where(c => c.ID == 2).FirstOrDefault<CV>();
+            return View(model);
         }
     }
 }
