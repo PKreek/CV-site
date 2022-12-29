f392888eb11d9f82a2ad4172a6fa6e6d1ea59d57
noahjarvback <noah.jarvback@hotmail.com>
2022-12-29 14:32:11 +0100

    indentering

--- CV-Site MVC/Controllers/ProjectController.cs
+++ CV-Site MVC/Controllers/ProjectController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CV_Site_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CV_Site_MVC.Controllers
 {
     public class ProjectController : Controller
     {
+
+        private SiteContext _dbContext;
+
+        public ProjectController(SiteContext dbContext)
+        {
+                _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
