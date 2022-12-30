f5aff1394581898c23ab06b2e13e9a878e93b33d
markwest7 <markus.westberg@gmail.com>
2022-12-30 08:43:24 +0100

    nästan uppdatera cv men också "#fuckuni

--- CV-Site MVC/Controllers/CVController.cs
+++ CV-Site MVC/Controllers/CVController.cs
@@ -13,33 +13,51 @@ namespace CV_Site_MVC.Controllers
         {
             _dbContext = dbContext;
         }
+
+        [HttpGet]
         public IActionResult CV()
         {
             CvViewModel model = new CvViewModel();
 
             model.UserID = currentUserId();
 
-            CV cv = _dbContext.cVs.Where(c => c.UserID == currentUserId()).FirstOrDefault<CV>();
+            CV cv = _dbContext.cVs.Where(c => c.UserID.Equals(currentUserId())).FirstOrDefault<CV>();
 
             if (cv != null) model.Cv = cv;
-            else model.Cv = new CV();
+            else
+            {
+                model.Cv = new CV(currentUserId());
+                _dbContext.cVs.Add(model.Cv);
+                _dbContext.SaveChanges();
+            }
 
             return View(model);
         }
 
-        public IActionResult NewCV(CV cv)
-        {
-            cv.UserID = currentUserId();
-            _dbContext.cVs.Add(cv);
-            _dbContext.SaveChanges();
-            return RedirectToAction("CV");
-        }
+        //public IActionResult NewCV(CV cv)
+        //{
+        //    cv.UserID = currentUserId();
+        //    _dbContext.cVs.Add(cv);
+        //    _dbContext.SaveChanges();
 
+        //    return RedirectToAction("CV");
+        //}
+
+        [HttpGet]
         public IActionResult Edit(int id)
         {
-            CV cv = _dbContext.cVs.Find(id);
+            CV cv = _dbContext.cVs.FirstOrDefault(c => c.ID.Equals(id));
 
             return View(cv);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CV cv)
+        {
+            _dbContext.cVs.Update(cv);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index","Home");
         }
 
         private string currentUserId()
