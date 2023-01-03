a8ab44ebc91964f31960ac90a66fb27faa41e464
PKreek <patrick.kreek@gmail.com>
2023-01-03 21:42:53 +0100

    Post, Put

--- CV-SiteAPI/Controllers/MessageController.cs
+++ CV-SiteAPI/Controllers/MessageController.cs
@@ -10,27 +10,47 @@ using Microsoft.AspNetCore.Mvc;
 namespace CV_SiteAPI.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController] 
+    [ApiController]
     public class MessageController : ControllerBase
     {
-            private SiteContext _siteContext;
-            public MessageController(SiteContext siteContext)
-            {
-                _siteContext = siteContext;
-            }
+        private SiteContext _siteContext;
+        public MessageController(SiteContext siteContext)
+        {
+            _siteContext = siteContext;
+        }
 
-            [HttpGet]
-            public List<Message> Get()
-            {
-                return _siteContext.Messages.ToList();
-            }
+        [HttpGet]
+        public List<Message> Get()
+        {
+            return _siteContext.Messages.ToList();
+        }
 
-            [HttpGet("{ID}")]
-            public Message Get(int ID)
-            {
-                return _siteContext.Messages.Find(ID);
+        [HttpGet("{ID}")]
+        public Message Get(int ID)
+        {
+            return _siteContext.Messages.Find(ID);
 
+        }
+
+        [HttpPost]
+        public void Post(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _siteContext.Messages.Add(message);
+                _siteContext.SaveChanges();
             }
         }
+
+        [HttpPut("{id}")]
+        public void Put([FromBody] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _siteContext.Messages.Update(message);
+                _siteContext.SaveChanges();
+            }
+        }
+    }
 }
 
