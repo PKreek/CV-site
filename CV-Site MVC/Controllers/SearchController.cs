79db40f43d8d03356c9973399040ee579323227c
NikAntKar <niklas.karlsson12345@hotmail.com>
2023-01-03 11:00:47 +0100

    Start på sök

---
+++ CV-Site MVC/Controllers/SearchController.cs
@@ -0,0 +1,94 @@
+﻿using CV_Site_MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CV_Site_MVC.Controllers
+{
+    public class SearchController : Controller
+    {
+        private SiteContext _dbContext;
+
+        public SearchController(SiteContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: SearchController
+        public ActionResult Index(string search)
+        {
+            
+                return View(_dbContext.cVs.Where(x => x.FirstName.StartsWith(search) || search == null).ToList());
+            
+        }
+
+            // GET: SearchController/Details/5
+            public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SearchController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SearchController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SearchController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SearchController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SearchController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SearchController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
