90d85abaedf47a9f9bd12fb113a09f0099335114
PKreek <patrick.kreek@gmail.com>
2023-01-03 21:18:27 +0100

    Create MessageController.cs

---
+++ CV-SiteAPI/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CV_SiteAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CV_SiteAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class MessageController : ControllerBase
+    {
+            private SiteContext _siteContext;
+            public MessageController(SiteContext siteContext)
+            {
+                _siteContext = siteContext;
+            }
+
+            [HttpGet]
+            public List<Message> Get()
+            {
+                return _siteContext.Messages.ToList();
+            }
+
+            [HttpGet("{ID}")]
+            public Message Get(int ID)
+            {
+                return _siteContext.Messages.Find(ID);
+
+            }
+        }
+}
+
