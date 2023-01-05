97a94f1f97b0a8ed401841769df8afa7e0a8626f
noahjarvback <noah.jarvback@hotmail.com>
2023-01-05 17:10:50 +0100

    Meddelande

--- CV-SiteAPI/Models/Message.cs
+++ CV-SiteAPI/Models/Message.cs
@@ -1,21 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace CV_SiteAPI.Models
 {
     public class Message
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public int Id { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
         public string SentTo { get; set; }
         public string SentFrom { get; set; }
+        public bool Read { get; set; }  
 
-        [ForeignKey(nameof(SentTo))]
-        public virtual User Message_Reciever  { get; set; }
+        //[ForeignKey(nameof(SentTo))]
+        //public virtual User Message_Reciever  { get; set; }
 
-        [ForeignKey(nameof(SentFrom))]
-        public virtual User Message_Sender { get; set; }
+        //[ForeignKey(nameof(SentFrom))]
+        //public virtual User Message_Sender { get; set; }
     }
 }
