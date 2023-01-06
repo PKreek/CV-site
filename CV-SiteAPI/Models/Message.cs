8bf19ef89dd1336f97cfdab46b04c556ef6b534e
markwest7 <markus.westberg@gmail.com>
2023-01-06 18:11:37 +0100

    Revert "Ändrar databasen"
    This reverts commit 1339d9b55eed7b5d16b27f99e84f9e502421eac2.

--- CV-SiteAPI/Models/Message.cs
+++ CV-SiteAPI/Models/Message.cs
@@ -6,17 +6,16 @@ namespace CV_SiteAPI.Models
 {
     public class Message
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public int Id { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
         public string SentTo { get; set; }
         public string SentFrom { get; set; }
+        public bool Read { get; set; }  
 
-        public bool Read { get; set; }
-
-        [ForeignKey(nameof(SentTo))]
-        public virtual User Message_Reciever { get; set; }
+        //[ForeignKey(nameof(SentTo))]
+        //public virtual User Message_Reciever  { get; set; }
 
         //[ForeignKey(nameof(SentFrom))]
         //public virtual User Message_Sender { get; set; }
